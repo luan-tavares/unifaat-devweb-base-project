a083d51a4121a099465431bb3a18df3057e0041e
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-05-22 20:27:15 -0300

    update, delete and get

--- bootstrap/constants.js
+++ bootstrap/constants.js
@@ -3,6 +3,7 @@ export default {
     HTTP: {
         SUCCESS: 200,
         SUCCESS_CREATED: 201,
+        SUCCESS_NO_CONTENT: 204,
         BAD_REQUEST: 400,
         NOT_FOUND: 404,
         SERVER_ERROR: 500
--- routes/exampleApi.js
+++ routes/exampleApi.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import ListExampleController from '../app/Controllers/ModelExample/ListExampleController.js';
 import CreateExampleController from '../app/Controllers/ModelExample/CreateExampleController.js';
+import GetExampleController from '../app/Controllers/ModelExample/GetExampleController.js';
+import UpdateExampleController from '../app/Controllers/ModelExample/UpdateExampleController.js';
+import DeleteExampleController from '../app/Controllers/ModelExample/DeleteExampleController.js';
 
 export default (function () {
 
@@ -11,8 +14,17 @@ export default (function () {
     // GET Listar
     router.get('/api/example', ListExampleController);
 
+    // GET Obter um
+    router.get('/api/example/:id', GetExampleController);
+
     // POST Criar
     router.post('/api/example', CreateExampleController);
+
+    // PUT Atualizar
+    router.put('/api/example/:id', UpdateExampleController);
+
+    // DELETE Atualizar
+    router.delete('/api/example/:id', DeleteExampleController);
 
     return router;
 
